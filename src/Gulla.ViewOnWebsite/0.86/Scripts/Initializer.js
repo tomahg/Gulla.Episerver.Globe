4b6a3cb8f0fecb8c21cc25b9564a871ed39caaea
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2019-10-25 10:34:48 +0200

    Initial commit :globe_with_meridians:

---
+++ src/Gulla.ViewOnWebsite/0.86/Scripts/Initializer.js
@@ -0,0 +1,15 @@
+﻿//viewonwebsite/Initializer.js
+define([
+    'dojo/_base/declare',
+    "epi/dependency",
+    "viewonwebsite/CustomToolbarProvider"
+], function (declare, dependency, CustomToolbarProvider) {
+    return declare(null, {
+        initialize: function () {
+            var commandregistry = dependency.resolve("epi.globalcommandregistry");
+            //The first parameter is the "area" that your provider should add commands to
+            //For the global toolbar the area is "epi.cms.globalToolbar"
+            commandregistry.registerProvider("epi.cms.globalToolbar", new CustomToolbarProvider());
+        }
+    });
+});
---
+++ src/Gulla.ViewOnWebsite/0.86/Scripts/ViewOnWebsiteCommand.js
@@ -0,0 +1,40 @@
+﻿//viewonwebsite/ViewOnWebsiteCommand.js
+define([
+        "dojo/_base/declare",
+        "epi/shell/command/_Command",
+        "dojo/topic",
+        "epi/dependency"
+    ],
+    function (declare, _Command, topic, dependency) {
+        return declare([_Command],
+            {
+                name: "ViewOnWebsite",
+                iconClass: "epi-iconWebsite",
+                canExecute: true,
+
+                constructor: function () {
+                    var contextService = dependency.resolve("epi.shell.ContextService");
+                    var currentContext = contextService.currentContext;
+                    this._contextChanged(currentContext);
+
+                    topic.subscribe("/epi/shell/context/changed", this._contextChanged);
+                },
+
+                _contextChanged: function (newContext) {
+                    if (!newContext) {
+                        return;
+                    }
+
+                    viewOnWebsite = newContext.publicUrl;
+                },
+
+                _execute: function () {
+                    if (viewOnWebsite) {
+                        window.open(viewOnWebsite);
+                    } else {
+                        alert('Content has no public URL.');
+                    }
+                }
+            });
+    }
+);
