b1a387d60333a1365a07393bb3ca36d3ae6c0574
Tomas Hensrud Gulla <tomas.h.gulla@novacare.no>
2019-10-25 21:07:18 +0200

    Show different icon for content without public url :lock:

---
+++ src/Gulla.ViewOnWebsite/0.92/Scripts/CustomToolbarProvider.js
@@ -0,0 +1,31 @@
+﻿//viewonwebsite/CustomToolbarProvider
+define([
+        "dojo/_base/declare",
+        "dijit/form/Button",
+        "epi-cms/component/command/_GlobalToolbarCommandProvider",
+        "viewonwebsite/ViewOnWebsiteCommand",
+        "dojo/topic",
+        "dojo/_base/lang"
+    ],
+    function (declare, Button, _GlobalToolbarCommandProvider, ViewOnWebsiteCommand, topic, lang) {
+        return declare([_GlobalToolbarCommandProvider],
+            {
+                constructor: function () {
+                    this.inherited(arguments);
+                    this.addToTrailing(new ViewOnWebsiteCommand(), { showLabel: false, widget: Button });
+                    topic.subscribe("/epi/shell/context/changed", lang.hitch(this, this._contextChanged));
+                },
+
+                // Executes when the context changes, i.e. nodes in the page tree are clicked
+                // Show correct icon and tooltip on after context change
+                _contextChanged: function (newContext) {
+                    if (!newContext) {
+                        return;
+                    }
+
+                    this.commands[0].set('iconClass', newContext.publicUrl ? "epi-iconWebsite" : "epi-iconLock");
+                    this.commands[0].set('label', newContext.publicUrl ? "View on website" : "Content has no URL");
+                } 
+            });
+    }
+);
---
+++ src/Gulla.ViewOnWebsite/0.92/Scripts/ViewOnWebsiteCommand.js
@@ -0,0 +1,38 @@
+﻿//viewonwebsite/ViewOnWebsiteCommand.js
+define([
+        "dojo/_base/declare",
+        "epi/shell/command/_Command",
+        "epi/dependency",
+        "dojo/topic"
+    ],
+    function (declare, _Command, dependency, topic) {
+        return declare([_Command],
+            {
+                name: "ViewOnWebsite",
+                iconClass: "epi-iconWebsite",
+                canExecute: true,
+
+                // Show correct icon and tooltip on initial page load
+                constructor: function () {
+                    var contextService = dependency.resolve("epi.shell.ContextService");
+                    var currentContext = contextService.currentContext;
+
+                    if (currentContext) {
+                        this.set('iconClass', currentContext.publicUrl ? "epi-iconWebsite" : "epi-iconLock");
+                        this.set('label', currentContext.publicUrl ? "View on website" : "Content has no URL");
+                    }
+                },
+
+                // Executes when toolbar button is clicked
+                _execute: function () {
+                    var contextService = dependency.resolve("epi.shell.ContextService");
+                    var currentContext = contextService.currentContext;
+                    var publicUrl = currentContext.publicUrl;
+
+                    if (publicUrl) {
+                        window.open(publicUrl);
+                    }
+                }
+            });
+    }
+);
